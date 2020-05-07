1493b1c20e6c612de8e18e8adddc1420f7073f5e
Daniel Erat <dan@erat.org>
2020-05-07 17:39:26 -0400

    More improvements.

--- print.go
+++ print.go
@@ -22,37 +22,45 @@ func main() {
 	}
 }
 
-func ts(tags []string) map[string]struct{} {
-	ts := make(map[string]struct{})
+// tagSet holds a set of HTML tag names.
+type tagSet map[string]struct{}
+
+func newTagSet(tags []string) tagSet {
+	ts := make(tagSet)
 	for _, t := range tags {
 		ts[t] = struct{}{}
 	}
 	return ts
 }
 
-// Void elements per https://html.spec.whatwg.org/multipage/syntax.html.
-// https://www.w3.org/TR/2011/WD-html-markup-20110405/syntax.html#syntax-elements lists a few more.
-var voidTags = ts(strings.Fields("area base br col embed hr img input link meta param source track wbr"))
-
-// Elements that don't nest their contents.
-// The first child instead appears immediately after the opening tag.
-// Space-only text nodes are also preserved within these elements.
-var inlineTags = ts(strings.Fields("a b code em i span s strong"))
-
-func isInline(n *html.Node) bool {
+// has returns true if n's tag is contained in ts.
+// Returns false if n is nil.
+func (ts tagSet) has(n *html.Node) bool {
 	if n == nil || n.Type != html.ElementNode {
 		return false
 	}
-	_, ok := inlineTags[n.Data]
+	_, ok := ts[n.Data]
 	return ok
 }
 
+// Void elements per https://html.spec.whatwg.org/multipage/syntax.html.
+// https://www.w3.org/TR/2011/WD-html-markup-20110405/syntax.html#syntax-elements lists a few more.
+var voidTags = newTagSet(strings.Fields("area base br col embed hr img input link meta param source track wbr"))
+
+// Elements that appear inline.
+// No newline is added before the element or after it.
+// Contents are not also not nested: The first child instead appears immediately after
+// the opening tag, and the last child appears immediately after the closing tag.
+// Spaces in text nodes adjacent to these tags are preserved.
+var inlineTags = newTagSet(strings.Fields("a b code em i span s strong"))
+
 // Non-void elements whose closing tags are omitted.
 // Similar to inline tags, these tags also don't nest their contents.
-var omitCloseTags = ts(strings.Fields("li"))
+// A newline is printed at the point where the closing tag would have appeared, though.
+var omitCloseTags = newTagSet(strings.Fields("li"))
 
-// Elements whose text should be preserved unchanged.
-var literalTags = ts(strings.Fields("pre script style"))
+// Elements whose contents should be preserved unchanged.
+var literalTags = newTagSet(strings.Fields("pre script style"))
 
 type printer struct {
 	w   io.Writer
@@ -92,13 +100,19 @@ func (p *printer) doc(n *html.Node) error {
 	return nil
 }
 
+// text handles the supplied node of type html.ElementNode.
 func (p *printer) element(n *html.Node) {
-	inline := isInline(n)
+	if n.Type != html.ElementNode {
+		panic(fmt.Sprintf("Got non-element node %q (type %v)", n.Data, n.Type))
+	}
+
+	inline := inlineTags.has(n)
 	if !inline && p.literalDepth == 0 && !p.lineStart {
 		p.endl()
 	}
 
 	tag := n.Data
+	p.indent()
 	p.write("<" + tag)
 	for _, a := range n.Attr {
 		p.write(" " + a.Key)
@@ -106,18 +120,19 @@ func (p *printer) element(n *html.Node) {
 			// Just escape double-quotes.
 			// TODO: Ambiguous ampersands (/&[a-zA-Z0-9]+;/) are also disallowed, but I'm ignoring
 			// those for now. See https://html.spec.whatwg.org/multipage/syntax.html#syntax-attributes.
-			p.writef(`="%s"`, strings.ReplaceAll(a.Val, `"`, `&quot;`))
+			escaped := strings.ReplaceAll(a.Val, `"`, `&quot;`)
+			p.write(`="` + escaped + `"`)
 		}
 	}
 	p.write(">")
 
-	_, literal := literalTags[tag]
-	_, omitClose := omitCloseTags[tag]
+	literal := literalTags.has(n)
+	omitClose := omitCloseTags.has(n)
 	if !inline && !literal && !omitClose {
 		p.endl()
 	}
 
-	if _, void := voidTags[tag]; void {
+	if voidTags.has(n) {
 		return
 	}
 
@@ -145,7 +160,8 @@ func (p *printer) element(n *html.Node) {
 		p.endl()
 	}
 
-	if _, omit := omitCloseTags[tag]; !omit {
+	if !omitClose {
+		p.indent()
 		p.write("</" + tag + ">")
 	}
 	if !inline && !literal {
@@ -156,14 +172,24 @@ func (p *printer) element(n *html.Node) {
 var spaceAroundNewline *regexp.Regexp = regexp.MustCompile("(?s)[ \t]*\n[ \t]*")
 var repeatedSpace *regexp.Regexp = regexp.MustCompile("  +")
 
+// text handles the supplied node of type html.TextNode.
 func (p *printer) text(n *html.Node) {
+	if n.Type != html.TextNode {
+		panic(fmt.Sprintf("Got non-text node %q (type %v)", n.Data, n.Type))
+	}
+
+	// TODO: Can this actually happen?
+	if len(n.Data) == 0 {
+		return
+	}
+
 	if p.literalDepth > 0 {
 		p.write(n.Data)
 		return
 	}
 
-	// Collapse whitespace for an inline formatting context as described under
-	// "How does CSS process whitespace?" in
+	// Collapse whitespace for an inline formatting context roughly following the process
+	// described in "How does CSS process whitespace?" in
 	// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Whitespace.
 	s := n.Data
 	s = spaceAroundNewline.ReplaceAllString(s, "\n")
@@ -171,37 +197,41 @@ func (p *printer) text(n *html.Node) {
 	s = strings.ReplaceAll(s, "\n", " ")
 	s = repeatedSpace.ReplaceAllString(s, " ")
 
-	if !isInline(n.PrevSibling) {
+	if !inlineTags.has(n.PrevSibling) {
 		s = strings.TrimLeft(s, " ")
 	}
-	if !isInline(n.NextSibling) {
+	if !inlineTags.has(n.NextSibling) {
 		s = strings.TrimRight(s, " ")
 	}
 
 	if s != "" {
+		p.indent()
 		p.write(s)
 	}
 }
 
-func (p *printer) writef(format string, args ...interface{}) {
-	p.write(fmt.Sprintf(format, args...))
-}
-
-func (p *printer) write(s string) {
-	if p.err != nil {
-		return
-	}
+// indent writes the proper amount of whitespace if lineStart is true and literalDepth is 0.
+func (p *printer) indent() {
 	if p.lineStart && p.literalDepth == 0 {
-		s = strings.Repeat(p.indentStr, p.level) + s
-		p.lineStart = false
+		p.write(strings.Repeat(p.indentStr, p.level))
 	}
-	_, p.err = io.WriteString(p.w, s)
 }
 
+// endl terminates the current line by writing a newline and setting lineStart to true.
+// It does nothing if lineStart was already true.
 func (p *printer) endl() {
 	if p.lineStart {
 		return
 	}
 	p.write("\n")
 	p.lineStart = true
+}
+
+// write outputs s and sets lineStart to false.
+func (p *printer) write(s string) {
+	if p.err != nil {
+		return
+	}
+	_, p.err = io.WriteString(p.w, s)
+	p.lineStart = false
 }
